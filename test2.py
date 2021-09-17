061883fd7d867567a688de748b228b7b76d7267e
RRRdik <32777@ma-web.nl>
2021-09-17 10:05:39 +0200

    mijn eerste commit

---
+++ test2.py
@@ -0,0 +1,13 @@
+print("Hello You! Ik ben Ronel.")
+print("Wie ben jij?")
+name = input("Jouw naam: ")
+print("Hello! " + name)
+import datetime
+
+x = datetime.datetime.now()
+
+print("De tijd en datum zijn:")
+print(x)
+
+
+input()
